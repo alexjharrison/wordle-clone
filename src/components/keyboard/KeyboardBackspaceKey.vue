8cfaaeaef08d56fff1d639856b327000f7265013
Alex Harrison <alexjharrison@gmail.com>
2022-02-05 18:06:06 -0500

    fix svg loading

--- src/components/keyboard/KeyboardBackspaceKey.vue
+++ src/components/keyboard/KeyboardBackspaceKey.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="keyboard-key" @click="handleBackspace">
-    <p>&lt;==</p>
+    <BackspaceIcon class="icon" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { useGameStore } from "@/composables/game";
+import { BackspaceIcon } from "@heroicons/vue/outline";
 
 const { handleBackspace } = useGameStore();
 </script>
+
+<style scoped>
+.keyboard-key {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.icon {
+  color: var(--key-text-1);
+}
+</style>
