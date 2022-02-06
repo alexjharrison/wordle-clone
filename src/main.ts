24063688b4abd786546b120c520a30c6d7af800b
Alex Harrison <alexjharrison@gmail.com>
2022-02-05 20:09:00 -0500

    register service worker

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import { useRegisterSW } from "virtual:pwa-register/vue";
+
+const intervalMS = 60 * 60 * 1000;
+
+useRegisterSW({
+  onRegistered(r) {
+    r &&
+      setInterval(() => {
+        r.update();
+      }, intervalMS);
+  },
+});
+
 createApp(App).mount("#app");
