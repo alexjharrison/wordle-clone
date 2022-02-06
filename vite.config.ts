673e752f2008638cf024db2dda76d245d75f22d7
Alex Harrison <alexjharrison@gmail.com>
2022-02-05 20:15:23 -0500

    delete any maskable icon pic

--- vite.config.ts
+++ vite.config.ts
@@ -31,12 +31,12 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
+          // {
+          //   src: "android-chrome-512x512.png",
+          //   sizes: "512x512",
+          //   type: "image/png",
+          //   purpose: "any maskable",
+          // },
         ],
       },
     }),
